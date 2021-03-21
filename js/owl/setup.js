084c20bc87451b7e3bb6ca0bced6a14cf33f029a
DiogoBarrosx <diogobarrosx@gmail.com>
2021-03-21 16:34:40 -0300

    Recriando a interface da Netflix

---
+++ js/owl/setup.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:12,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        840:{
+            items:4
+        },
+        1000:{
+            items:5
+        },
+        1250:{
+            items:6
+        }
+    }
+})
