41c24211050c40b03a045f99f51f04df01d4a658
DiogoBarrosCode <diogobarros.code@gmail.com>
2023-01-24 23:32:16 -0300

    estrutura de rotas

--- movies-app/src/App.tsx
+++ movies-app/src/App.tsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom';
 import './App.css'
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>Movies Project</h1>
+      <Outlet />
     </div>
   )
 }
--- movies-app/src/main.tsx
+++ movies-app/src/main.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Home from './routes/Home'
 import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
