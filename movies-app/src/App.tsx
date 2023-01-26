885e80daead43450e04a1885b7803e604607a687
DiogoBarrosCode <diogobarros.code@gmail.com>
2023-01-26 00:33:29 -0300

    estrutura da api TMDB + axios

--- movies-app/src/App.tsx
+++ movies-app/src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import './App.css'
+import Navbar from './components/navbar/Navbar';
 
 function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <Outlet />
     </div>
   )
--- movies-app/src/routes/Home.tsx
+++ movies-app/src/routes/Home.tsx
@@ -1,11 +1,39 @@
+import axios from 'axios';
 import React from 'react';
-import Navbar from '../components/navbar/Navbar';
+import { useState, useEffect } from 'react';
+
+const apiKey = "api_key=aa412de9cb6448a62e521ae48ef4817d";
+const baseUrl = "https://api.themoviedb.org/3/movie";
+const imgUrl = "https://image.tmdb.org/t/p/w500/";
+const genresList = "https://api.themoviedb.org/3/genre/movie/list?";
+const movieGenre = "https://api.themoviedb.org/3/discover/movie?";
 
 const Home = () => {
+    
+    const [topMovies, setTopMovies] = useState([]);
+   
+    const listGenres = async (url: string) => {
+
+        try {
+            
+            const response = await axios.get(url)
+            const data = await response.data;
+            console.log(data)
+
+        } catch (error) {
+           console.log(error) 
+        }
+    
+    };
+
+    useEffect(() => {
+        const listGenresMovies = `${genresList}${apiKey}`
+        console.log(listGenresMovies)
+        listGenres(listGenresMovies)
+    }, [])
+
     return (
-        <>
-            <Navbar />
-        </>
+        <h2>Filmes</h2>
     )
 }
 
