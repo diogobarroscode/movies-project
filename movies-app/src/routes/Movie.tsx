700aad0e0eb58408cbec310508400c79ea4077cc
DiogoBarrosCode <diogobarros.code@gmail.com>
2023-02-07 02:11:00 -0300

    pagina de filmes com mais informaçoes

--- movies-app/src/routes/Movie.tsx
+++ movies-app/src/routes/Movie.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import axios from 'axios';
 import {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
-import CardMovie from "../components/card/CardMovie";
 import './Movie.css';
+import CoverMovie from "../components/cover/CoverMovies";
 
 const apiKey = "api_key=aa412de9cb6448a62e521ae48ef4817d";
 const baseUrl = "https://api.themoviedb.org/3/movie/";
+const imgUrl = "https://image.tmdb.org/t/p/w500/";
+const imgUrl2 = "https://image.tmdb.org/t/p/w1280/";
 
 
 const Movie = () => {
     
     const {id} = useParams();
-    const [movie, setMovie] = useState(null);
+    const [movie, setMovie] = useState<any>([]);
 
     const getMovie = async (url: string) => {
 
@@ -36,12 +38,27 @@ const Movie = () => {
     }, [])
     
     return (
-        <div className="movie-page">
-            <div className="movie">{movie && (
-                <div className="info-movie">
-                    <CardMovie movie={movie}/>
+        <div className="movie-page" style={{backgroundImage: `url(${imgUrl2 + movie.backdrop_path})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", height: "100vh"}}>
+
+           <div className="movie">
+                <div className="movie-folder">
+                    <img src={imgUrl + movie.poster_path} alt={movie.title} />
                 </div>
-            )}</div>
+                <div className="movie-info">
+                    <h1>{movie.title}</h1>
+                    <span>
+                        <ul>
+                            <li>Duração: {movie.runtime} min</li>
+                        </ul>
+                    </span>
+
+                    <span className="more-info">
+                        <h2>Sinopse</h2>
+                        <p className="sinopse">{movie.overview}</p>
+                    </span>
+                </div>
+                
+           </div>
         </div>
 
     )
