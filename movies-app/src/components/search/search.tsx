376173533f1b64f2c23310118555f199427c95a0
DiogoBarrosCode <diogobarros.code@gmail.com>
2023-04-09 21:16:01 -0300

    implemented uncontrolled form

--- movies-app/src/components/search/search.tsx
+++ movies-app/src/components/search/search.tsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 import './search.css';
 
 const Search = () => {
+
+    const [search, setSearch] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+
+        if(!search) return
+        navigate(`/search?q=${search}`);
+        setSearch("");
+    };
+
     return(
         <div className='nav-box'>
-        <form>
-            <input type="text" placeholder='Buscar' />
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='Buscar' 
+            onChange={(e) => setSearch(e.target.value)} 
+            value = {search}
+            />
             <button type='submit'><FaSearch /></button>
         </form>
     </div>
