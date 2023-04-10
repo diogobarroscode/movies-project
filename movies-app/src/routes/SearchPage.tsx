57bf4c100f95f56f4001984508fc7dabf5287fe8
DiogoBarrosCode <diogobarros.code@gmail.com>
2023-04-09 22:39:00 -0300

    impemented query string from API search

--- movies-app/src/routes/SearchPage.tsx
+++ movies-app/src/routes/SearchPage.tsx
@@ -1,5 +1,7 @@
+import axios from 'axios';
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import CardMovie from '../components/card/CardMovie';
 
 
 const searchURL = import.meta.env.VITE_API_SEARCH;
@@ -10,11 +12,40 @@ const SearchPage = () => {
     const [searchParams] = useSearchParams();
     const query = searchParams.get("q");
 
-    const [movies, setMovies] = useState([]);
+    const [searchMovies, setSearchMovies] = useState([]);
+
+    const getSearchMovies = async (url: string) => {
+
+        try {
+            const response = await axios.get(url);
+            const data = await response.data;
+            console.log(data);
+            setSearchMovies(data.results);
+
+        } catch (error) {
+           console.log(error) 
+        };
+        
+    };
+
+    useEffect(() => {
+
+        const getSearchList = `${searchURL}${apiKey}&query=${query}&language=pt-BR`
+        getSearchMovies(getSearchList);
+        
+    }, [query]);
 
 
     return(
-        <div>Resultados para: {query}</div>
+        <div>
+            <div>Resultados para: {query}</div>
+        
+            <div className='container-movie-genre'>
+                {searchMovies?.map((movie: any) => <CardMovie key={movie.id} movie={movie} />)}
+            </div>
+
+        </div>
+
     );
 };
 
