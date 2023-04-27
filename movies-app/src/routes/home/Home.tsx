391202c9cdd771a396b236c7af94765717a1789c
DiogoBarrosCode <diogobarros.code@gmail.com>
2023-04-26 23:53:05 -0300

    responsive implementation for home page

--- movies-app/src/routes/home/Home.tsx
+++ movies-app/src/routes/home/Home.tsx
@@ -69,7 +69,7 @@ const Home = () => {
             effect= {"fade"}
             slidesPerView={1} loop 
             navigation
-            pagination
+            pagination={false}
             autoplay={{
               delay: 7000,
               disableOnInteraction: false,
@@ -101,9 +101,102 @@ const Home = () => {
                       <Swiper slidesPerView={1} spaceBetween={10} loop modules={[Navigation]} navigation 
                         breakpoints={{
                           320: {
-                            slidesPerView: 2,
+                            slidesPerView: 4,
+                            spaceBetween: 80,
+                          },
+                          360: {
+                            slidesPerView: 4,
+                            spaceBetween: 70,
+                          },
+                          460: {
+                            slidesPerView: 4,
+                            spaceBetween: 66,
+                          },
+                          520: {
+                            slidesPerView: 4,
+                            spaceBetween: 60,
+                          },
+                          560: {
+                            slidesPerView: 4,
+                            spaceBetween: 55,
+                          },
+                          600: {
+                            slidesPerView: 5,
+                            spaceBetween: 120,
+                          },
+                          680: {
+                            slidesPerView: 5,
+                            spaceBetween: 100,
+                          },
+                          699: {
+                            slidesPerView: 5,
+                            spaceBetween: 75,
+                          },
+                          750: {
+                            slidesPerView: 5,
+                            spaceBetween: 50,
+                          },
+                          820: {
+                            slidesPerView: 5,
+                            spaceBetween: 20,
+                          },
+                          860: {
+                            slidesPerView: 6,
+                            spaceBetween: 100,
+                          },
+                          950: {
+                            slidesPerView: 6,
+                            spaceBetween: 70,
+                          },
+                          1040: {
+                            slidesPerView: 6,
+                            spaceBetween: 50,
+                          },
+                          1141: {
+                            slidesPerView: 7,
+                            spaceBetween: 120,
+                          },
+                          1200: {
+                            slidesPerView: 7,
+                            spaceBetween: 90,
+                          },
+                          1260: {
+                            slidesPerView: 7,
+                            spaceBetween: 70,
+                          },
+                          1310: {
+                            slidesPerView: 7,
                             spaceBetween: 30,
-                          }
+                          },
+                          1351: {
+                            slidesPerView: 8,
+                            spaceBetween: 120,
+                          },
+                          1430: {
+                            slidesPerView: 8,
+                            spaceBetween: 90,
+                          },
+                          1570: {
+                            slidesPerView: 8,
+                            spaceBetween: 50,
+                          },
+                          1600: {
+                            slidesPerView: 8,
+                            spaceBetween: 20,
+                          },
+                          1620: {
+                            slidesPerView: 9,
+                            spaceBetween: 120,
+                          },
+                          1690: {
+                            slidesPerView: 9,
+                            spaceBetween: 90,
+                          },
+                          1780: {
+                            slidesPerView: 9,
+                            spaceBetween: 70,
+                          },
+                          
                         }}
                       
                       >
