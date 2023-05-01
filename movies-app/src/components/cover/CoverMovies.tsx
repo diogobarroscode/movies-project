59ef9def2e3584e853c1ad010c76ebed4dbbe902
DiogoBarrosCode <diogobarros.code@gmail.com>
2023-05-01 18:41:00 -0300

    add info and buttons for slides in homepage

--- movies-app/src/components/cover/CoverMovies.tsx
+++ movies-app/src/components/cover/CoverMovies.tsx
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom";
+import { FaPlay } from "react-icons/fa";
+import { BsInfoCircle } from "react-icons/bs"
+import { MdOutlineMovieFilter } from "react-icons/md"
 import './CoverMovie.css';
 import { Movie } from "../../types/Interfaces"
 
@@ -6,16 +9,37 @@ interface Props {
     movie: Movie;
   }
 
-const imgUrl = import.meta.env.VITE_IMG_URL_FULL;
+const imgUrlFull = import.meta.env.VITE_IMG_URL_FULL;
 
 const CoverMovie = ({movie}:Props) => {
     return (
 
         <div className="cover-movie">
 
-            {<Link to={`/movie/${movie.id}`}>
-                <img src={imgUrl + movie.backdrop_path} alt={movie.title} />
-            </Link>}
+            
+
+                <div className="movie-info-cover">
+
+                    {<Link to={`/movie/${movie.id}`}>
+                        <h1>{movie.title}</h1>
+                    </Link>}
+
+                    <span><MdOutlineMovieFilter />Lançamento</span>
+
+                    <p>{movie.overview}</p>
+
+                    <div className="buttons">
+                        <button className="trailer"><FaPlay />Trailer</button>
+                        {<Link to={`/movie/${movie.id}`}>
+                            <button className="more"><BsInfoCircle />Mais informações</button>
+                        </Link>}
+                    </div>
+                    
+                </div>
+
+                <img src={imgUrlFull + movie.backdrop_path} alt={movie.title} />
+
+            
             
         </div>
     )
